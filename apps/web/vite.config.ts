96cd540f49064756869ae50c0c398c60a6adbda1
TheVirginBrokey <1687551+waptik@users.noreply.github.com>
2025-07-27 14:44:27 +0000

    chore(web): replace vite to rolldown-vite

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,14 +1,16 @@
-import tailwindcss from "@tailwindcss/vite"
-import { tanstackStart } from "@tanstack/react-start/plugin/vite"
-import viteReact from "@vitejs/plugin-react"
-import { defineConfig, loadEnv } from "vite"
-import tsconfigPaths from "vite-tsconfig-paths"
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react-oxc";
+import { defineConfig, loadEnv } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import pkg from "./package.json";
+
 
 export default defineConfig(({ mode }) => {
-	
-	const env = loadEnv(mode, process.cwd())
 
-	const isDevelopment = mode === "development"
+	const env = loadEnv(mode, process.cwd());
+
+	const isDevelopment = mode === "development";
 	return {
 		optimizeDeps: {
 			exclude: [
@@ -32,8 +34,18 @@ export default defineConfig(({ mode }) => {
 		plugins: [
 			tsconfigPaths(),
 			tailwindcss(),
-			tanstackStart({ target: "cloudflare-module", customViteReactPlugin: true }),
+			tanstackStart({
+				target: "cloudflare-module",
+				customViteReactPlugin: true,
+			}),
 			viteReact(),
 		],
-	}
-})
+		define: {
+			APP_VERSION: JSON.stringify(pkg.version),
+		},
+	};
+});
+
+declare global {
+	const APP_VERSION: string;
+}
